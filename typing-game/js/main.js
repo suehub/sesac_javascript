d4703cac3dccc76903e1d669ce7414aa553b11ea
sue <dldustn2040@gmail.com>
2022-11-17 21:37:34 +0900

    api

--- typing-game/js/main.js
+++ typing-game/js/main.js
@@ -6,17 +6,43 @@ const messageDisplay = document.querySelector("#message")
 
 const GAME_TIME = 5;    // 변하지 않는 변수 선언 시 대문자와 언더바 사용
 
+const API_URL = "https://random-word-api.herokuapp.com/word?number=100";
+
 let words = ["banana", "key", "car", "javascript", "scalper"];
 let score = 0;
 let time = 0;
 let timeInterval;
 let isPlaying = false;  // interval이 쌓이는 것을 막기 위한 변수
+let isReady = false;    // 통신이 되기 전에 start가 될 때를 방지하기 위한 변수
 
 
-wordInput.addEventListener("input", e=>{ // 타자를 치는 행위
+init()  // html 랜더링하면서 javascript import하고 init()을 최초로 실행해서 데이터가 세팅됨
+
+// // promise 문법. 초기화 함수를 init() 이름으로 많이 사용함
+// function init(){
+//     // promise 문법
+//     const res = fetch(API_URL).then(res => res.json()).then((data) => words = data);    // json 변환한 값을 넘겨줌
+//     // console.log(res)    // promise 출력
+
+// }
+
+// async await 문법 : 명령 실행 후 기다리고 완료가 되면 다음 명령을 실행함
+async function init(){  // async function(비동기 함수) : callback과 promise의 단점을 보완하기 위해 추가됨
+    const res = await fetch(API_URL);   // fetch 함수가 다 실행된 후에 값이 담김
+    const data = await res.json();
+    words = data.filter(item => item.length < 7) // filter는 배열 함수
+    isReady = true;
+    console.log(words)
+    // 둘 다 사용가능하지만 async await 이 최근 많이 사용됨
+}
+
+
+
+
+wordInput.addEventListener("input", e => { // 타자를 치는 행위
     const typedText = e.target.value;    // input창의 value
     const currentText = currentWord.innerText;
-    if(typedText.toUpperCase() === currentText.toUpperCase()) { // 대문자 변환 후 비교
+    if(typedText.toUpperCase() === currentText.toUpperCase() && isReady) { // 대문자 변환 후 비교
         addScore()
         setNewWord()
     }
